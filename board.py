c33c2127c8d9cbdc6f33190b61bf6e8828d8b5bf
Steve Meyer <smeyer@oberlin.edu>
2016-11-15 00:07:30 -0500

    oops

--- board.py
+++ board.py
@@ -8,7 +8,6 @@ class Board:
             try:
                 self.moves = moves
                 for key, val in moves.items():
-                    self.board[val[0]][val[1]] = key
                     if ((val[0],val[1]) != (-1,-1)):
                         self.board[val[0]][val[1]] = key
                     self.turn += 1
