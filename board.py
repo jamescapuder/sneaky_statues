4f6e925dc386a345d0774ac9729ddc9b1af9db00
Steve Meyer <smeyer@oberlin.edu>
2016-11-14 22:07:20 -0500

    adding turn count as a board class var

--- board.py
+++ board.py
@@ -5,19 +5,21 @@
 
 class Board:
     def __init__(self, moves=None):
-        self.board = [[0],[0,0],[0,0,0],[0,0,0,0],[0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-        self.pieces = ["1","2","3","4","5","6","7","8"]
+        self.board = [[0], [0, 0], [0,0,0], [0,0,0,0], [0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0,0]]
+        self.pieces = ["1", "2", "3", "4", "5", "6", "7", "8"]
+        self.turn = 1
         if moves:
             try:
                 self.moves = moves
-                for k,v in moves.items():
-                    self.board[v[0]][v[1]] = k
+                for key, val in moves.items():
+                    self.board[val[0]][val[1]] = key
+                    self.turn += 1
             except IndexError:
                 print("Invalid board positions, edit input and try again")
         else:
             self.moves = {}
             for j in self.pieces:
-                self.moves[j] = (-1,-1)
+                self.moves[j] = (-1, -1)
 
     def get_board(self):
         return self.board
@@ -28,8 +30,9 @@ class Board:
         try:
             templocat = self.moves[piece]
             self.board[templocat[0]][templocat[1]] = 0
-            self.moves[piece] = (x,y)
+            self.moves[piece] = (x, y)
             self.board[x][y] = piece
+            self.turn += 1
         except IndexError:
             print("Attempt to set position out of board bounds.")
 
@@ -39,24 +42,24 @@ class Board:
         if next_piece in self.pieces:
             templocat = self.moves[next_piece]
             possible_boards = []
-            for i in range(0,7):
+            for i in range(0, 7):
                 for j in range(len(self.board[i])):
-                    if (i, j)!=templocat and self.board[i][j]==0:
+                    if (i, j) != templocat and self.board[i][j] == 0:
                         temp = Board(self.moves)
                         temp.set_position(next_piece, i, j)
-                        possible_boards.append(((i,j),temp))
+                        possible_boards.append(((i, j), temp))
             return possible_boards
         else:
             print("Invalid piece number")
 
     def __str__(self):
-        printstr=""
-        for i in range(0,len(self.board)):
-            row=''
+        printstr = ""
+        for i in range(0, len(self.board)):
+            row = ''
             for j in self.board[i]:
-                row+=str(j)+" "
-            printstr+='{:^16}'.format(row)
-            printstr+='\n'
+                row += str(j)+" "
+            printstr += '{:^16}'.format(row)
+            printstr += '\n'
         return printstr
 
 if __name__=='__main__':
