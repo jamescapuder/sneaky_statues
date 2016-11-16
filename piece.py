58fbb5caf168a8f530be15fdfe6f54a2f14f17a6
Steve Meyer <smeyer@oberlin.edu>
2016-11-16 18:47:31 -0500

    forgot to commit piece

---
+++ piece.py
@@ -0,0 +1,22 @@
+
+def get_players(num):
+    if num % 2 == 1:
+        return "one", "two"
+    else:
+        return "two", "one"
+
+class Piece:
+    def __init__(self, num, xycord):
+        self.num = num
+        self.xy = xycord
+        self.x = xycord[0]
+        self.y = xycord[1]
+        self.player, self.opp = get_players(self.num)
+
+    def __str__(self):
+        return str(self.num) + " " + str(self.xy)
+    
+    # def __eq__(self, other):
+    #     return self.xy == other.xy
+
+
