1c2be8b11c6f1c611804f3c7aa569fd8e89c51dc
Steve Meyer <smeyer@oberlin.edu>
2016-11-18 21:42:21 -0500

    :zap:

--- main.py
+++ main.py
@@ -16,32 +16,6 @@ except NameError: # Python 3
     zero_depth_bases = (str, bytes, Number, range, bytearray)
     iteritems = 'items'
 
-class BoardTest(unittest.TestCase):
-    @unittest.skip("skipping")
-    def testBoard(self):
-        root = board.Board()
-        children = root.get_possible_moves("1")
-        for child in children:
-            print(child[0])
-            print(child[1])
-
-class TreeTest(unittest.TestCase):
-    #@unittest.skip("skipping")
-    def testTree(self):
-        print()
-        start = time.time()
-        x = deque([piece.Piece(1,(1,1)),piece.Piece(3,(2,2)),piece.Piece(5,(5,6)),piece.Piece(7,(1,2))],maxlen=4)
-        y = deque([piece.Piece(2,(4,4)),piece.Piece(4,(0,0)),piece.Piece(6,(2,4)),piece.Piece(8,(1,3))],maxlen=4)
-        players = {"one": x, "two": y}
-        root = board.Board(players)
-        root.find_children(1)
-        for child in root.children:
-            child.find_children(2)
-        print(repr(root))
-        print(getsize(root))
-        end = time.time()
-        print(end-start)
-
 def getsize(obj_0):
     """Recursively iterate to sum size of object & members."""
     def inner(obj, _seen_ids=set()):
@@ -64,9 +38,36 @@ def getsize(obj_0):
         return size
     return inner(obj_0)
 
-def tree(root, depth):
-    pass
+class BoardTest(unittest.TestCase):
+    #@unittest.skip("skipping")
+    def testMaxRun(self):
+        test_1 = [1, 2, 3, 4]
+        self.assertEqual(board.max_run(test_1), 4)
+        test_2 = [8, 2, 3, 4]
+        self.assertEqual(board.max_run(test_2), 3)
+        test_3 = [9, 1, 2, 8]
+        self.assertEqual(board.max_run(test_3), 2)
+        test_4 = [1, 3, 5, 7]
+        self.assertEqual(board.max_run(test_4), 1)
 
+class TreeTest(unittest.TestCase):
+    @unittest.skip("skipping")
+    def testTree(self):
+        print()
+        start = time.time()
+        x = deque([piece.Piece(1,(1,1)),piece.Piece(3,(2,2)),piece.Piece(5,(5,6)),piece.Piece(7,(1,2))],maxlen=4)
+        y = deque([piece.Piece(2,(4,4)),piece.Piece(4,(0,0)),piece.Piece(6,(2,4)),piece.Piece(8,(1,3))],maxlen=4)
+        players = {"one": x, "two": y}
+        root = board.Board(players)
+        root.find_children(1)
+        for child in root.children:
+            child.find_children(2)
+        print(repr(root))
+        print(getsize(root))
+        end = time.time()
+        print(end-start)
+        print(root)
+        
 def main():
     unittest.main()
 
